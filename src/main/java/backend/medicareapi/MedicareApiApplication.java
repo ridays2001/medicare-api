ad617ad889d044f7352d3a6975adaf207ea2a0a8
Riday <ridayshah2001@gmail.com>
2020-10-10 10:00:34 +0530

    Init: backend-api

---
+++ src/main/java/backend/medicareapi/MedicareApiApplication.java
@@ -0,0 +1,13 @@
+package backend.medicareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/medicareapi/MedicareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.medicareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
