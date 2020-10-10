124bd02fe5413e8ccd1ddef88b2cb6101fd62dcf
Riday <ridayshah2001@gmail.com>
2020-10-10 12:41:36 +0530

    Feat: user repo to manage the db

---
+++ src/main/java/backend/medicareapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package backend.medicareapi.repositories;
+
+import backend.medicareapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
