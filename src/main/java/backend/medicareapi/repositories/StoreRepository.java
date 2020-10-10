65cd8d33716781a1ea4883ee33c59a3053f2b8a5
Riday <ridayshah2001@gmail.com>
2020-10-10 16:54:27 +0530

    Feat: store repository
    - Manage the database

---
+++ src/main/java/backend/medicareapi/repositories/StoreRepository.java
@@ -0,0 +1,9 @@
+package backend.medicareapi.repositories;
+
+import backend.medicareapi.models.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+}
