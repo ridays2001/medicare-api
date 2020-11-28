47260112bdac3186911cb0936eaadbaa1d591db2
Riday <ridayshah2001@gmail.com>
2020-11-28 18:37:04 +0530

    Feat: added search by username

--- src/main/java/backend/medicareapi/repositories/UserRepository.java
+++ src/main/java/backend/medicareapi/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package backend.medicareapi.repositories;
 
 import backend.medicareapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query(value = "SELECT * FROM User WHERE username = :username AND ROWNUM = 1", nativeQuery = true)
+	User findByUsername(String username);
 }
