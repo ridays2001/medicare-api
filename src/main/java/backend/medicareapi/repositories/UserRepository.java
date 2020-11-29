b450118b13047c43c4893f61a4b0f78ed374fc5f
Riday <ridayshah2001@gmail.com>
2020-11-29 10:49:46 +0530

    Fix: use LIMIT instead of ROWNUM

--- src/main/java/backend/medicareapi/repositories/UserRepository.java
+++ src/main/java/backend/medicareapi/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	@Query(value = "SELECT * FROM User WHERE username = :username AND ROWNUM = 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM User WHERE username = :username LIMIT 1", nativeQuery = true)
 	User findByUsername(String username);
 }
